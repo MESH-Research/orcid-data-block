84b423ccc06becb47cb92aefe82d8aee1bdee41b
Brian Adams <wreality@gmail.com>
2024-02-29 14:26:50 -0500

    add lib

---
+++ lib/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '200d3b35e9ec5fa364d976f9cf9459dff4fa9c55',
+        'name' => 'mesh-research/orcid-data-block',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mesh-research/orcid-data-block' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '200d3b35e9ec5fa364d976f9cf9459dff4fa9c55',
+            'dev_requirement' => false,
+        ),
+    ),
+);
